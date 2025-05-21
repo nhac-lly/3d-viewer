991eed31fbe95209a7d2c329b80e48e72dcfa6b3
nhacly <nhacly.arobid@gmail.com>
2025-05-21 10:57:34 +0700

    first commit

---
+++ src/app/components/GltfViewer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import React, { Suspense } from "react";
+
+function Model() {
+  const gltf = useGLTF("/Qualcomm_Model_v1_fix02.gltf");
+  return <primitive object={gltf.scene} />;
+}
+
+export default function GltfViewer() {
+  return (
+    <div className="w-full h-full">
+      <Canvas camera={{ position: [2, 2, 2], fov: 50 }}>
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+} 
